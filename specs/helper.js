4eaf23c987479a1caea2332e27260098a7c3f00b
egordylan <egordylan@gmail.com>
2022-04-10 10:33:20 +0300

    refact: add timeout 10_000

--- specs/helper.js
+++ specs/helper.js
@@ -3,8 +3,8 @@ async function login (link) {
     await $('#login').setValue('walker@jw.com');
     await $('#password').setValue('password');
     await $('button').click();
-    await $('#spinner').waitForDisplayed({reverse: false, timeout: 5000});
-    await $('#spinner').waitForDisplayed({reverse: true, timeout: 5000});
+    await $('#spinner').waitForDisplayed({reverse: false, timeout: 10000});
+    await $('#spinner').waitForDisplayed({reverse: true, timeout: 10000});
 }   
 
 
